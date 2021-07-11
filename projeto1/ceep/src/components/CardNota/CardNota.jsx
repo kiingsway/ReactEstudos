5e83b7a866d0262796a43a2fbdb45fcb15910bc0
King Sway (Marcos Oranmiyan) <knucles0906@live.com>
2021-07-11 16:04:37 -0300

    Adicionando imagem de deletar nos Cards
    Aula 01. Deletando um Card - Atividade 03: Importando SVG

--- projeto1/ceep/src/components/CardNota/CardNota.jsx
+++ projeto1/ceep/src/components/CardNota/CardNota.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './estilo.css'
+import {ReactComponent as DeleteSVG} from '../../assets/img/delete.svg'
 
 class CardNota extends Component {
     render() { 
         return (
             <section className="card-nota shadow">
-                <header>
-                    <h3>{this.props.titulo}</h3>
+                <header className="card-nota-cabecalho">
+                    <h3 class="cabecalho">{this.props.titulo}</h3>
+                    <DeleteSVG />
                 </header>
                 <p>{this.props.nota}</p>
             </section>
